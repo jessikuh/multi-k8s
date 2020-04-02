383dc32e72f6590df8551ae54d83a76b1bcfd47a
Jessica Stamos <jstamos@envano.com>
2020-04-02 11:28:57 -0500

    Push deployment files

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker build -t jessikuh/multi-client:latest -t jessikuh/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jessikuh/multi-server:latest -t jessikuh/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jessikuh/multi-worker:latest -t jessikuh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jessikuh/multi-client:latest
+docker push jessikuh/multi-server:latest
+docker push jessikuh/multi-work:latest
+
+docker push jessikuh/multi-client:$SHA
+docker push jessikuh/multi-server:$SHA
+docker push jessikuh/multi-work:$SHA
+
+# APPLY A SPECIFIC HASH IN ORDER FOR K8S TO KNOW DOCKERFILE HAS UPDATED
+# OTHERWISE WILL CHOOSE LATEST AND THINK THERE'S NO UPDATE
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=jessikuh/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jessikuh/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=jessikuh/multi-worker:$SHA
